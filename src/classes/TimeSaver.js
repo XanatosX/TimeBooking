f9c3f90ed6b2563fcae7a823937aacbc7e5213a5
Simon Aberle <simon.aberle@mm-bit.de>
2019-05-20 15:52:41 +0200

    Adding class for timestamps

---
+++ src/classes/TimeSaver.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+class TimeSaver {
+
+  constructor(path) {
+    this.path = path;
+    this.startTimes = [];
+    this.endTimes = [];
+  }
+
+  addStartTime(datetime) {
+    this.startTimes.push(datetime);
+  }
+
+  addEndTime(datetime) {
+    this.endTimes.push(datetime);
+  }
+
+  getWorkTime() {
+    if (this.endTimes.lenght !== this.startTimes.lenght) {
+      return 0;
+    }
+
+    var passed = 0;
+    for (var i = 0; i < this.startTimes.length; i++) {
+      var startTime = this.startTimes[i];
+      var endTime = this.endTimes[i];
+
+      passed += endTime - startTime;
+    }
+
+    return passed;
+  }
+
+}
+
+module.exports = TimeSaver;
