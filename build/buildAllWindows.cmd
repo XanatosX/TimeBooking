5bc8706a53c31df0857281204cd44bee40b97778
Simon Aberle <simon.aberle@mm-bit.de>
2019-05-22 14:35:28 +0200

    Added windows build script

---
+++ build/buildAllWindows.cmd
@@ -0,0 +1,18 @@
+@echo off
+set myDirectory=%~dp0..
+set buildDirectory=builds/
+echo I'm working in %myDirectory%
+echo This script will install some npm packages as globals
+echo It will load all the dependencies for the project as well
+echo The following will be installed as global:
+echo electron-packager
+set /P DUMMY=press any key if this is okay, otherwise press ctrl + c to abort
+call npm install -g electron-packager
+cd %myDirectory%
+call npm install
+mkdir %buildDirectory%
+echo using %buildDirectory% to save the builds
+call electron-packager . TimeBooking --out=%buildDirectory% --platform=win32 --arch=x64  --ignore="/build" --ignore=%buildDirectory%
+call electron-packager . TimeBooking --out=%buildDirectory% --platform=win32 --arch=ia32 --ignore="/build" --ignore=%buildDirectory%
+call electron-packager . TimeBooking --out=%buildDirectory% --platform=linux --arch=x64  --ignore="/build" --ignore=%buildDirectory%
+
