d9b147d27e00da3bb2a8caa6dc709ca4655002bc
Simon Aberle <simonaberle@gmx.de>
2019-05-26 00:01:36 +0200

    Added missing JSDoc block

--- src/classes/SettingsManager.js
+++ src/classes/SettingsManager.js
@@ -1,7 +1,15 @@
 var fs = require('fs')
 var SettingsContainer = require('./SettingsContainer.js')
 
+/**
+ * This class will save and load settings container from the hdd
+ */
 class SettingsManager {
+  
+  /**
+   * This method if the constructor of the settings manager
+   * @param  {String} path
+   */
   constructor (path) {
     this.path = path
     this.path = path + '/settings/'
@@ -10,6 +18,11 @@ class SettingsManager {
     }
   }
 
+  /**
+   * This method will save data to a file on the disk
+   * @param  {String} name
+   * @param  {Object} data
+   */
   save (name, data) {
     fs.writeFileSync(this.path + name + '.json', data, 'utf8', (err) => {
       if (err) {
@@ -19,7 +32,11 @@ class SettingsManager {
     })
     return true
   }
-
+  
+  /**
+   * This method will load you a settings container from the disk
+   * @param  {String} name
+   */
   load (name) {
     let path = this.path + name + '.json'
     if (!fs.existsSync(path)) {
