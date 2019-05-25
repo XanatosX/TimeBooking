843c2ba4f5bbdaba1137d4d085b39896648ac945
Simon Aberle <simonaberle@gmx.de>
2019-05-25 23:58:53 +0200

    Adding missing JSDoc blocks

--- src/classes/SettingsContainer.js
+++ src/classes/SettingsContainer.js
@@ -1,12 +1,29 @@
+/**
+ * This class represents a setting container
+ */
 class SettingsContainer {
+
+  /**
+   * Construct an empty instance of thisclass
+   */
   constructor () {
     this.settings = {}
   }
 
+  /**
+   * This method will add a new setting to the container
+   * @param  {String} name
+   * @param  {String} data
+   */
   addSetting (name, data) {
     this.settings[name] = data
   }
 
+  
+  /**
+   * The method will return you the value of a setting withname
+   * @param  {String} name
+   */
   getSetting (name) {
     if (this.settings.name === undefined) {
       return null
@@ -14,10 +31,16 @@ class SettingsContainer {
     return this.settings.name
   }
 
+  /**
+   * This mehtod will return you the object containing the settings
+   */
   getObject () {
     return this.settings
   }
 
+  /**
+   * This function will get you a saveable string
+   */
   getWritable () {
     return JSON.stringify(this.settings)
   }
