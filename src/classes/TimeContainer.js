707167a2eba4c543aa889dd474696db98484108f
Simon Aberle <simonaberle@gmx.de>
2019-05-26 00:05:55 +0200

    Added missing JSDoc block

--- src/classes/TimeContainer.js
+++ src/classes/TimeContainer.js
@@ -1,25 +1,46 @@
 const TimeDataSet = require('./TimeDataSet.js')
 
+/**
+ * This class contains different time data sets
+ */
 class TimeContainer {
+
+  /**
+   * This is the constructor of the class and will create an empty instance
+   */
   constructor () {
     this.times = []
   }
 
+  /**
+   * This function will add a new time to the collection
+   * @param  {TimeDataSet} data
+   */
   addTime (data) {
     this.times.push(data)
   }
 
+  /**
+   * This method will return you all the times in thecontainer
+   */
   getTimes () {
     return this.times
   }
 
+  /**
+   * This method will delete a dataset at position x
+   * @param  {Int32Array id
+   */
   deleteDataSet (id) {
     this.times.splice(id, 1)
   }
 
+  /**
+   * This method will return you the current container as saveable String
+   */
   getWritable () {
-    var json = {}
-    var timingArray = []
+    let json = {}
+    let timingArray = []
     this.times.forEach(function (item) {
       timingArray.push(item.getSaveableDataSet())
     })
@@ -27,12 +48,15 @@ class TimeContainer {
     return JSON.stringify(json)
   }
 
+  /**
+   * This method will return you the whole working time for this container
+   */
   getCompleteWorkTime () {
     if (this.times.lenght === 0) {
       return 0
     }
 
-    var passed = 0
+    let passed = 0
     this.times.forEach(function (item) {
       passed += item.getWorkTime()
     })
@@ -40,6 +64,10 @@ class TimeContainer {
     return passed
   }
 
+  /**
+   * This method will fill the content of this instance with the data of the object
+   * @param {Object} data
+   */
   fromJson (data) {
     if (data.timings === undefined) {
       return
