d252c649315df4a46eba1093a040faf9b8339f40
Simon Aberle <simonaberle@gmx.de>
2019-06-30 22:56:46 +0200

    Adding semikolons

--- src/classes/Cookies.js
+++ src/classes/Cookies.js
@@ -8,29 +8,29 @@
    * @param  {electron} electron
    */
   constructor (electron) {
-    this.session = electron.remote.session.fromPartition('persist:name')
+    this.session = electron.remote.session.fromPartition('persist:name');
   }
   /**
-   * This mehtod will create a new cookie
+   * This method will create a new cookie
    * @param  {String} name
    * @param  {String} data
    */
   setCookie (name, data) {
-    let expiration = new Date()
-    let hour = expiration.getHours()
-    hour += 1
-    expiration.setHours(hour)
+    let expiration = new Date();
+    let hour = expiration.getHours();
+    hour += 1;
+    expiration.setHours(hour);
     let cookieData = {
       url: '/',
       name: name,
       value: data,
       expirationDate: expiration.getTime()
-    }
+    };
     this.session.cookies.set(cookieData, function (error) {
       if (error) {
-        console.log(error)
+        console.log(error);
       }
-    })
+    });
   }
 
   /**
@@ -41,24 +41,24 @@
   getCookie (name, callback) {
     let value = {
       name: name
-    }
+    };
     this.session.cookies.get(value, function (error, cookies) {
       if (error || cookies[0] === undefined || cookies[0].value === undefined) {
         if (error) {
-          console.log(error)
+          console.log(error);
         }
-        return
+        return;
       }
-      callback(null, cookies[0].value)
-    })
+      callback(null, cookies[0].value);
+    });
   }
 
   /**
    * This method will delete all the already saved cookies
    */
   clearCookies () {
-    this.session.clearStorageData({ storages: 'cookies' })
+    this.session.clearStorageData({ storages: 'cookies' });
   }
 }
 
-module.exports = Cookies
+module.exports = Cookies;
