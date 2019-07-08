7a2adbfadcb83ed43f2c5a4f5f89e66c8da83464
Simon Aberle <simonaberle@gmx.de>
2019-07-08 21:25:15 +0200

    Adding missing line end

--- src/scripts/addTime.js
+++ src/scripts/addTime.js
@@ -1,121 +1,121 @@
-const electron = require('electron')
-const ipcRenderer = electron.ipcRenderer
-const TimeFileManager = require('../classes/TimeFileManager.js')
-const TimeContainer = require('../classes/TimeContainer.js')
-const TimeDataSet = require('../classes/TimeDataSet.js')
-const remote = electron.remote
+const electron = require('electron');
+const ipcRenderer = electron.ipcRenderer;
+const TimeFileManager = require('../classes/TimeFileManager.js');
+const TimeContainer = require('../classes/TimeContainer.js');
+const TimeDataSet = require('../classes/TimeDataSet.js');
+const remote = electron.remote;
 
-var today = null
-var id = null
-var edit = false
+var today = null;
+var id = null;
+var edit = false;
 
 ipcRenderer.on('time', (event, message) => {
-  today = new Date(message)
-  var timeInput = document.getElementById('start-timestamp')
-  setTime(timeInput, today)
-})
+  today = new Date(message);
+  var timeInput = document.getElementById('start-timestamp');
+  setTime(timeInput, today);
+});
 
 ipcRenderer.on('id', (event, message) => {
-  id = message
-})
+  id = message;
+});
 
 ipcRenderer.on('edit', (event, message) => {
-  edit = true
-  var startInput = document.getElementById('start-timestamp')
-  var endInput = document.getElementById('end-timestamp')
-  var description = document.getElementById('description')
+  edit = true;
+  var startInput = document.getElementById('start-timestamp');
+  var endInput = document.getElementById('end-timestamp');
+  var description = document.getElementById('description');
 
-  var startTime = new Date(message.startTime)
-  today = startTime
-  var endTime = new Date(message.endTime)
+  var startTime = new Date(message.startTime);
+  today = startTime;
+  var endTime = new Date(message.endTime);
 
-  setTime(startInput, startTime)
-  setTime(endInput, endTime)
-  description.value = message.description
-})
+  setTime(startInput, startTime);
+  setTime(endInput, endTime);
+  description.value = message.description;
+});
 
 document.addEventListener('DOMContentLoaded', function () {
-  addListner()
-  addKeyPress()
-}, false)
+  addListener();
+  addKeyPress();
+}, false);
 
 function setTime (element, timeInput) {
-  var time = String(timeInput.getHours()).padStart(2, '0') + ':' + String(timeInput.getMinutes()).padStart(2, '0')
-  element.value = time
+  var time = String(timeInput.getHours()).padStart(2, '0') + ':' + String(timeInput.getMinutes()).padStart(2, '0');
+  element.value = time;
 }
 
 function addKeyPress () {
-  window.addEventListener('keyup', KeyUp, true)
+  window.addEventListener('keyup', KeyUp, true);
 }
 
 function KeyUp (event) {
   if (event.keyCode === 27) {
-    close()
+    close();
   }
   if (event.keyCode === 13) {
-    save()
+    save();
   }
 }
 
-function addListner () {
-  var closeBtn = document.getElementById('closeButton')
-  var saveBtn = document.getElementById('saveButton')
+function addListener () {
+  var closeBtn = document.getElementById('closeButton');
+  var saveBtn = document.getElementById('saveButton');
   closeBtn.addEventListener('click', function (event) {
-    close()
-  })
+    close();
+  });
   saveBtn.addEventListener('click', function (event) {
-    save()
-  })
-};
+    save();
+  });
+}
 
 function close () {
-  var window = remote.getCurrentWindow()
-  window.close()
+  var window = remote.getCurrentWindow();
+  window.close();
 }
 
 function save () {
-  var timeStart = document.getElementById('start-timestamp')
-  var timeEnd = document.getElementById('end-timestamp')
-  var description = document.getElementById('description')
-  var folder = remote.app.getPath('userData')
-  var manager = new TimeFileManager(folder, today)
-  var container = manager.loadTodayFile()
+  var timeStart = document.getElementById('start-timestamp');
+  var timeEnd = document.getElementById('end-timestamp');
+  var description = document.getElementById('description');
+  var folder = remote.app.getPath('userData');
+  var manager = new TimeFileManager(folder, today);
+  var container = manager.loadTodayFile();
   if (container === null) {
-    container = new TimeContainer()
+    container = new TimeContainer();
   }
 
-  var newDataSet = new TimeDataSet()
+  var newDataSet = new TimeDataSet();
 
   if (timeStart.value === undefined) {
-    return
+    return;
   }
 
-  var endTime = null
-  var startTime = 'T' + timeStart.value + ':00'
+  var endTime = null;
+  var startTime = 'T' + timeStart.value + ':00';
   if (timeEnd.value !== undefined) {
-    endTime = 'T' + timeEnd.value + ':00'
+    endTime = 'T' + timeEnd.value + ':00';
   }
 
-  var date = String(today.getFullYear()) + '-'
-  date += String(today.getMonth() + 1).padStart(2, '0') + '-'
-  date += String(today.getDate()).padStart(2, '0')
-  var startStamp = new Date(date + startTime)
-  var endStamp = new Date(date + endTime)
-  newDataSet.setStartTime(startStamp)
+  var date = String(today.getFullYear()) + '-';
+  date += String(today.getMonth() + 1).padStart(2, '0') + '-';
+  date += String(today.getDate()).padStart(2, '0');
+  var startStamp = new Date(date + startTime);
+  var endStamp = new Date(date + endTime);
+  newDataSet.setStartTime(startStamp);
   if (endStamp !== null) {
-    newDataSet.setEndTime(endStamp)
+    newDataSet.setEndTime(endStamp);
   }
-  newDataSet.setDescription(description.value)
+  newDataSet.setDescription(description.value);
 
   if (edit) {
     if (id === null) {
-      console.log('Missing id')
-      return
+      console.log('Missing id');
+      return;
     }
-    container.deleteDataSet(id)
+    container.deleteDataSet(id);
   }
-  container.addTime(newDataSet)
-  manager.saveFile(container.getWritable())
+  container.addTime(newDataSet);
+  manager.saveFile(container.getWritable());
 
-  close()
+  close();
 }
