a1614989dccc50ac7afe4e4ef94207d00e67bb91
Simon Aberle <10531466+XanatosX@users.noreply.github.com>
2021-01-20 23:21:23 +0100

    adding documentation

--- src/js/classes/translation/Language.js
+++ src/js/classes/translation/Language.js
@@ -1,22 +1,46 @@
+/**
+ * This class represents a single language
+ */
 class Language {
 
+    /**
+     * Create a new instance of this class
+     */
     constructor() {
         this.data = [];
         this.name = "Undefined";
     }
 
+    /**
+     * Set the name of the language
+     * @param {string} name 
+     */
     setName(name) {
         this.name = name;
     }
 
+    /**
+     * get the name of the language
+     * @returns string
+     */
     getName() {
         return this.name;
     }
 
+    /**
+     * Add a translation for the language
+     * @param {string} key 
+     * @param {string} value 
+     */
     addTranslation(key, value) {
         this.data[key] = value;
     }
 
+    /**
+     * Get the translation for a specific translation
+     * @param {string} key 
+     * @returns string
+     */
     getTranslation(key) {
         if (this.data[key] === undefined) {
             return null;
