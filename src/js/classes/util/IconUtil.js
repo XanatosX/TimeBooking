04bf0eda1f32185038445f816ff150070c178b18
Simon Aberle <10531466+XanatosX@users.noreply.github.com>
2021-01-29 18:02:18 +0100

    adding icon utils

---
+++ src/js/classes/util/IconUtil.js
@@ -0,0 +1,42 @@
+const { icon } = require("@fortawesome/fontawesome-svg-core");
+
+let iconUtil = null;
+class IconUtil {
+
+    constructor() {
+        this.basePath = null;
+        this.mode = null;
+        this.reset();
+    }
+
+    reset() {
+        this.basePath = this.getDefaultPath();
+        this.setMode("light");
+    }
+
+    setBasePath(newBasePath) {
+        this.basePath = newBasePath;
+    }
+
+    getDefaultPath() {
+        return "../../assets/"
+    }
+
+    setMode(mode) {
+        this.mode = mode;
+    }
+
+    isDark(isDark) {
+        if (isDark) {
+            this.setMode("dark");
+        }
+        this.setMode("light");
+    }
+
+    getIcon(name) {
+        console.log(this.basePath + this.mode + "/" + name);
+        return this.basePath + this.mode + "/" + name;
+    }
+}
+iconUtil = iconUtil === null ? new IconUtil() : iconUtil;
+module.exports = iconUtil;
